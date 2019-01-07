2877def632f6141f6304daf8be42a89b96728a71
Trond Svandal <trond@cdynamics.no>
2019-01-07 14:21:13 +0100

    First Commit

---
+++ Increment/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace Increment
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView txtNumber;
+
+        int number;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            AppCenter.Start("e60f3e39-b3e3-4048-8fbc-18964f0196d2",
+                   typeof(Analytics), typeof(Crashes));
+
+
+
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            txtNumber = FindViewById<TextView>(Resource.Id.txtNumber);
+
+            FindViewById<Button>(Resource.Id.btnIncrement).Click += (e, o) =>
+            txtNumber.Text = (++number).ToString();
+
+            FindViewById<Button>(Resource.Id.btnDecrement).Click += (e, o) =>
+            txtNumber.Text = (--number).ToString();
+
+
+        }
+    }
+}
